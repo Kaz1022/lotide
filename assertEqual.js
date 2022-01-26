0ee49c28a11c18352db6b3d08284507204a39ca2
Kazuko Baynton <kazukobaynton@gmail.com>
2022-01-26 14:57:40 -0800

    assertEqual.js first commit

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
