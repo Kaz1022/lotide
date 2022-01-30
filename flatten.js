fd050816c26702e5dc843a097589da19d27973b6
Kazuko Baynton <kazukobaynton@gmail.com>
2022-01-29 17:55:13 -0800

    flatten.js first commit

---
+++ flatten.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = function(array) {
+  let finalArr = [];
+ 
+  for (let i = 0; i < array.length; i++) {
+    if (!Array.isArray(array[i])) {
+      finalArr.push(array[i]);
+    } else if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        finalArr.push(array[i][j]);
+      }
+    }
+  }
+  return finalArr;
+};
+  
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
