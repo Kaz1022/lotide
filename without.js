0edf8f7ac3fa82d4540d4162ddfe421d6ef1e007
Kazuko Baynton <kazukobaynton@gmail.com>
2022-01-29 16:25:44 -0800

    without.js first commit

---
+++ without.js
@@ -0,0 +1,45 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let itemsToKeep = [];
+  for (let i = 0; i < source.length; i++) {
+    if (itemsToRemove.indexOf(source[i]) === -1) {
+      itemsToKeep.push(source[i]);
+    }
+  }
+  return itemsToKeep;
+};
+
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+
+assertArraysEqual(without([1, 2, 3], [2]), [1, 3]);
+assertArraysEqual(without([1, 2, 3], [3]), [1, 2]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+
+
+
