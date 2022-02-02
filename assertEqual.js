ded4bb5e45a74a35d05229f1c14a5bec266357f2
Kazuko Baynton <kazukobaynton@gmail.com>
2022-02-01 16:49:47 -0800

    adding countOnly.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+    return console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
   }
+  console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
 };
 
 
 assertEqual("Bootcamp", "Bootcamp");
 assertEqual(1, 1);
+assertEqual(1, 12);
