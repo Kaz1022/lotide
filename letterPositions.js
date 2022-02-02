baab91175d69a196dc02119a2b37ae25c9b54e5e
Kazuko Baynton <kazukobaynton@gmail.com>
2022-02-02 14:49:43 -0800

    letterPositions.js first commit

---
+++ letterPositions.js
@@ -0,0 +1,52 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return console.log(`👍👍👍Assertion Passed: [${actual}] === [${expected}]`);
+  }
+  console.log(`❌❌❌Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+
+assertArraysEqual(letterPositions("lighthouse in the house").l, [0]);
+assertArraysEqual(letterPositions("lighthouse in the house").i, [1, 11]);
+assertArraysEqual(letterPositions("lighthouse in the house").g, [2]);
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
+assertArraysEqual(letterPositions("lighthouse in the house").t, [4, 14]);
+assertArraysEqual(letterPositions("lighthouse in the house").o, [6, 19]);
+assertArraysEqual(letterPositions("lighthouse in the house").u, [7, 20]);
+assertArraysEqual(letterPositions("lighthouse in the house").s, [8, 21]);
+assertArraysEqual(letterPositions("lighthouse in the house").e, [9, 16, 22]);
+assertArraysEqual(letterPositions("lighthouse in the house").n, [12]);
