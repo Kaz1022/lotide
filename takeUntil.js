e9da294a4195081437735de7c48a77cc20f714f4
Kazuko Baynton <kazukobaynton@gmail.com>
2022-02-05 09:27:18 -0800

    takeUntil.js refactored

--- takeUntil.js
+++ takeUntil.js
@@ -21,16 +21,15 @@ const assertArraysEqual = function(actual, expected) {
 const takeUntil = function(array, callback) {
   const results = [];
   for (const element of array) {
-    results.push(element);
     if (callback(element)) {
       break;
     }
+    results.push(element);
   }
-  results.pop();
   return results;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data1 = [1, 2, 3, -4, 5, 6, 7];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
 
@@ -41,5 +40,5 @@ const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
 // assertArraysEqual(takeUntil(), []);
-assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 3]);
 assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
