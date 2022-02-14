21a37b300f1a8cfa98ead9bfbb95c3867c0d1197
Kazuko Baynton <kazukobaynton@gmail.com>
2022-02-14 13:49:33 -0800

    refactored assertArraysEqual.js and created test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -18,5 +7,4 @@ const assertArraysEqual = function(actual, expected) {
   console.log(`❌❌❌Assertion Failed: [${actual}] !== [${expected}]`);
 };
 
-// assertArraysEqual("1,1,1", [1,1,1]);
-// assertArraysEqual([1,1,1], [1,1,1]);
+module.exports = assertArraysEqual;
