4260cdc2367bbd9be71788d11da8c30341ba2e3f
Kazuko Baynton <kazukobaynton@gmail.com>
2022-02-15 15:56:23 -0800

    installed mocha and chai, refactored test files

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,21 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result[0], ["Lighthouse", "Labs"][0]);
+// assertEqual(result[1], ["Lighthouse", "Labs"][1]);
+// assertEqual(result.length, 2);
+
+// using mocha and chai
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result[0], ["Lighthouse", "Labs"][0]);
-assertEqual(result[1], ["Lighthouse", "Labs"][1]);
-assertEqual(result.length, 2);
+describe("#tail", () => {
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+
+  it("returns 'Labs' for ['Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Lighthouse', 'Labs']), ['Labs']);
+  });
+});
