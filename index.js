51ff5cd698a5a6ebf11bc466c8dd6ee667c60d18
Kazuko Baynton <kazukobaynton@gmail.com>
2022-02-16 20:59:47 -0800

    implement property value shorthand syntax from ES6 in index.js

--- index.js
+++ index.js
@@ -9,30 +9,30 @@ const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
 const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
-
+//implement property value shorthand syntax from ES6
 module.exports = {
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  eqObjects: eqObjects,
-  eqArrays: eqArrays;
-  countletters: countLetters,
-  countOnly: countOnly,
-  findKey: findKey,
-  findKeyByValue = findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  map: map,
-  takeUntil: takeUntil,
-  without: without  
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  eqObjects,
+  eqArrays,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  head,
+  tail,
+  middle,
+  map,
+  takeUntil,
+  without
 };
